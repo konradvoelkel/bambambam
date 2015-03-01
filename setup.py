5912b23080ca998cea80ecfa0b5b2191edf61378
konradvoelkel <web@konradvoelkel.de>
2015-03-01 19:41:43 +0100

    using setuptools soon

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bambambam',
+    version = '0.5',
+    install_requires=[
+        "pygame",
+    ],
+    description='Teach toddlers human-computer interaction in a fun way',
+    author='Konrad Voelkel',
+    author_email='bambambam-at-konradvoelkel.com',
+    license = 'public domain',
+    url = 'http://www.github.com/konradvoelkel/bambambam',
+    package_dir = {'': 'src'},
+#    packages = ['bambambam'],
+    packages = find_packages(),
+    package_data = {
+        'bambambam-data': ['data/*.*'],
+        'bambambam-audio-letters': ['data/audio-letters/*.*'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: Public Domain',
+        'Topic :: Games/Entertainment',
+        'Topic :: Education :: Computer Aided Instruction (CAI)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    use_2to3 = False,
+)
